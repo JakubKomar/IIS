7398443448a9c35582c20f04d3ebe7ad391420c8
komca12 <57370991+komca12@users.noreply.github.com>
2021-11-08 23:00:58 +0100

    formular update

--- app/LoginModule/Presenters/SingUpPresenter.php
+++ app/LoginModule/Presenters/SingUpPresenter.php
@@ -25,11 +25,14 @@ final class SignUpPresenter extends \App\CoreModule\Presenters\BasePresenter
 	protected function createComponentSignUpForm(): Form
 	{
 		$form = new Form;
+		$form->addProtection();
 		$form->addText('username', 'Login:')->setRequired('Zadejte login, prosím.');
-
-		$form->addPassword('password', 'Heslo:')->setRequired('Zadejte heslo, prosím.');
-		$form->addPassword('password2', 'Zopakujte heslo:')->setRequired('Zadejte heslo znovu, prosím.');
-
+		$form->addPassword('password', 'Heslo:')->setRequired('Zadejte heslo, prosím.')->addRule($form::MIN_LENGTH, 'Heslo musí mít alespoň %d znaků', 8);
+		$form->addPassword('passwordVerify', 'Heslo pro kontrolu:')
+			->setRequired('Zadejte prosím heslo ještě jednou pro kontrolu')
+			->addRule($form::EQUAL, 'Hesla se neshodují', $form['password'])
+			->setOmitted();
+		$form->addEmail('email', 'E-mail');
 		$form->addSubmit('send', 'Registrovat');
 
 
@@ -40,19 +43,16 @@ final class SignUpPresenter extends \App\CoreModule\Presenters\BasePresenter
 
 	public function signUpFormSucceeded(Form $form, \stdClass $values): void
 	{
-		if($values->password!=$values->password2)
-			$form->addError('Hesla nejsou stejná');
-		else
-		{
-			try 
-			{
-				$this->MyAuthenticator->add($values->username, $values->password);
-				$this->redirect(':Core:Homepage:');
 
-			} catch (DuplicateNameException $e) 
-			{
-				$form->addError('Uživatel exituje, zvolte prosím jiný login');
-			}
+		try 
+		{
+			$this->MyAuthenticator->add($values->username, $values->password);
+			$this->redirect(':Core:Homepage:');
+
+		} catch (DuplicateNameException $e) 
+		{
+			$form->addError('Uživatel exituje, zvolte prosím jiný login');
 		}
+		
 	}
 }
